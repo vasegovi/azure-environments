6cc898a11ea51ca82698e535e74fd02a077a73e5
Vanessa Segovia <vasegovi@microsoft.com>
2022-07-08 22:41:58 -0500

    testing without assigment

--- terraform/synapse/main.tf
+++ terraform/synapse/main.tf
@@ -14,22 +14,12 @@ data "azurerm_subscription" "primary" {
 data "azurerm_client_config" "synapseconf" {
 }
 
-resource "azurerm_role_assignment" "roleOwner" {
-  scope                = azurerm_storage_account.storagesynapse.id
-  role_definition_name = "Owner"
-  principal_id         = data.azurerm_client_config.synapseconf.object_id
-  timeouts {
-    create = "1m"
-    delete = "1m"
-  }
-}
+
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "synapsefsys" {
   name               = var.filesystemname
   storage_account_id = azurerm_storage_account.storagesynapse.id
-  depends_on = [
-    azurerm_role_assignment.roleOwner
-  ]  
+   
 }
 
 resource "azurerm_synapse_workspace" "synapsewsp" {
