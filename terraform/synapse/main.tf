4847b74a7820f0d21458cb179eecbf66cb9e7fe1
Vanessa Segovia <vasegovi@microsoft.com>
2022-07-11 00:00:07 -0500

    integration of spark pool with variables

--- terraform/synapse/main.tf
+++ terraform/synapse/main.tf
@@ -14,12 +14,22 @@ data "azurerm_subscription" "primary" {
 data "azurerm_client_config" "synapseconf" {
 }
 
-
+resource "azurerm_role_assignment" "roleOwner" {
+  scope                = azurerm_storage_account.storagesynapse.id
+  role_definition_name = "Owner"
+  principal_id         = data.azurerm_client_config.synapseconf.object_id
+  timeouts {
+    create = "1m"
+    delete = "1m"
+  }
+}
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "synapsefsys" {
   name               = var.filesystemname
   storage_account_id = azurerm_storage_account.storagesynapse.id
-   
+  depends_on = [
+    azurerm_role_assignment.roleOwner
+  ]
 }
 
 resource "azurerm_synapse_workspace" "synapsewsp" {
@@ -38,5 +48,29 @@ resource "azurerm_synapse_workspace" "synapsewsp" {
 
   identity {
     type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_synapse_sql_pool" "sqlpoolsynapse" {
+  name                 = var.sqlpoolsynapsename
+  synapse_workspace_id = azurerm_synapse_workspace.synapsewsp.id
+  sku_name             = var.sku_name
+  create_mode          = "Default"
+}
+
+resource "azurerm_synapse_spark_pool" "sparkpoolsynapse" {
+  name                 = var.sparkpoolsynapsename
+  synapse_workspace_id = azurerm_synapse_workspace.synapsewsp.id
+  node_size_family     = var.nodesizefamily
+  node_size            = var.nodesize
+  cache_size           = 100
+
+  auto_scale {
+    max_node_count = var.maxnodecount
+    min_node_count = var.minnodecount
+  }
+
+   auto_pause {
+    delay_in_minutes = 15
   }
 }
